ae1207a47560b5cf94ead3462d3960715ba50903
15sFR5007TU (205Q) <Sarthakhule123@gmail.com>
2023-01-20 21:10:09 +0530

    TASK 4 COMPLETED

---
+++ app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,29 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Where you are watching this video?",
+            "Which company owns the Apple?",
+            "Which company internship?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"Oasis","Google","Facebook","Sarthak"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Apple",
+            "Oasis"
+    };
+
+
+}
